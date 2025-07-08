05ffb654b643f821983226e5851e35cef303b7ed
TusharDayal1711 <tushar.dayal@remotestate>
2025-07-08 18:37:23 +0530

    create register for public user

--- database/dbhelper/registerPublicUser.go
+++ database/dbhelper/registerPublicUser.go
@@ -1,5 +1,57 @@
 package dbhelper
 
-func publicRegister(username) error {
+import (
+	"database/sql"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+)
 
+func IsUserExists(tx *sqlx.Tx, email string) (bool, error) {
+	var id uuid.UUID
+	err := tx.QueryRow(`
+		SELECT id FROM users 
+		WHERE email = $1 AND archived_at IS NULL
+	`, email).Scan(&id)
+	if err == sql.ErrNoRows {
+		return false, nil
+	}
+	if err != nil {
+		return false, fmt.Errorf("failed to check existing user: %w", err)
+	}
+	return true, nil
+}
+
+func InsertIntoUser(tx *sqlx.Tx, username, email string) (uuid.UUID, error) {
+	var id uuid.UUID
+	err := tx.QueryRow(`
+		INSERT INTO users (username, email) 
+		VALUES ($1, $2) RETURNING id
+	`, username, email).Scan(&id)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("failed to insert user: %w", err)
+	}
+	return id, nil
+}
+
+func InsertIntoUserRole(tx *sqlx.Tx, userId uuid.UUID, role string, createdBy uuid.UUID) error {
+	_, err := tx.Exec(`
+		INSERT INTO user_roles (role, user_id, created_by)
+		VALUES ($1, $2, $3)
+	`, role, userId, createdBy)
+	if err != nil {
+		return fmt.Errorf("failed to insert user role: %w", err)
+	}
+	return nil
+}
+
+func InsertIntoUserType(tx *sqlx.Tx, userId uuid.UUID, empType string, createdBy uuid.UUID) error {
+	_, err := tx.Exec(`
+		INSERT INTO user_type (type, user_id, created_by)
+		VALUES ($1, $2, $3)
+	`, empType, userId, createdBy)
+	if err != nil {
+		return fmt.Errorf("failed to insert user type: %w", err)
+	}
+	return nil
 }
---
+++ handler/publicRegister.go
@@ -0,0 +1,93 @@
+package handler
+
+import (
+	"inventory_management_system/database"
+	"inventory_management_system/database/dbhelper"
+	"inventory_management_system/models"
+	"inventory_management_system/utils"
+	"net/http"
+	"strings"
+
+	"github.com/google/uuid"
+	jsoniter "github.com/json-iterator/go"
+)
+
+func publicRegister(w http.ResponseWriter, r *http.Request) {
+	var userReq models.PublicUserReq
+	if err := utils.ParseJSONBody(r, &userReq); err != nil {
+		utils.RespondError(w, http.StatusBadRequest, err, "invalid input")
+		return
+	}
+
+	// Validate email
+	if userReq.Email == "" {
+		utils.RespondError(w, http.StatusBadRequest, nil, "email is required")
+		return
+	}
+	splitEmail := strings.Split(userReq.Email, "@")
+	if len(splitEmail) != 2 || splitEmail[1] != "remotestate.com" {
+		utils.RespondError(w, http.StatusBadRequest, nil, "only remotestate.com domain is valid")
+		return
+	}
+	usernameParts := strings.Split(splitEmail[0], ".")
+	if len(usernameParts) != 2 {
+		utils.RespondError(w, http.StatusBadRequest, nil, "failed to get username from email")
+		return
+	}
+	username := usernameParts[0] + " " + usernameParts[1]
+
+	//transaction
+	tx, err := database.DB.Beginx()
+	if err != nil {
+		utils.RespondError(w, http.StatusInternalServerError, err, "failed to start transaction")
+		return
+	}
+
+	var userId uuid.UUID
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
+		}
+	}()
+
+	// checking if user with same email already exit
+	isExists, err := dbhelper.IsUserExists(tx, userReq.Email)
+	if err != nil {
+		utils.RespondError(w, http.StatusInternalServerError, err, "failed to check existing user")
+		return
+	}
+	if isExists {
+		utils.RespondError(w, http.StatusBadRequest, nil, "email already registered")
+		return
+	}
+
+	//insertin new user in users table with username and email
+	userId, err = dbhelper.InsertIntoUser(tx, username, userReq.Email)
+	if err != nil {
+		utils.RespondError(w, http.StatusInternalServerError, err, "failed to create user")
+		return
+	}
+	const defaultRole = "employee"
+	const defaultType = "full_time"
+
+	if err = dbhelper.InsertIntoUserRole(tx, userId, defaultRole, userId); err != nil {
+		utils.RespondError(w, http.StatusInternalServerError, err, "failed to assign role")
+		return
+	}
+
+	if err = dbhelper.InsertIntoUserType(tx, userId, defaultType, userId); err != nil {
+		utils.RespondError(w, http.StatusInternalServerError, err, "failed to assign type")
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	jsoniter.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "account created successfully",
+		"userId":  userId,
+	})
+}
