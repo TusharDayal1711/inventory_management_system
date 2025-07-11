c5fd06ab1a66e85381738b6271f05aceb1d0fcfa
TusharDayal1711 <tushar.dayal@remotestate>
2025-07-11 13:05:16 +0530

    user models, will change in future

--- models/users.go
+++ models/users.go
@@ -3,3 +3,15 @@ package models
 type PublicUserReq struct {
 	Email string `json:"email"`
 }
+
+type EmployeeUpdateReq struct {
+	Name      string `json:"name,omitempty"`
+	ContactNo string `json:"contact_no,omitempty"`
+}
+
+type ManagerRegisterReq struct {
+	Username  string `json:"username" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+	ContactNo string `json:"contact_no" validate:"required"`
+	Type      string `json:"type" validate:"required,oneof=full_time intern freelancer"`
+}
