2e87238d83baf6f14785a5ac7c2173f9aa5837fc
TusharDayal1711 <tushar.dayal@remotestate>
2025-07-11 12:00:26 +0530

    add fuction to add asset to users

---
+++ database/dbhelper/manageAssets.go
@@ -0,0 +1,41 @@
+package dbhelper
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+)
+
+func AssignAssetByID(tx *sqlx.Tx, assetId uuid.UUID, userId uuid.UUID, assignedBy uuid.UUID) error {
+
+	var exists int
+	err := tx.Get(&exists, `
+		SELECT 1 FROM asset_assign 
+		WHERE asset_id = $1 AND archived_at IS NULL 
+		LIMIT 1
+	`, assetId)
+	if err == nil {
+		return fmt.Errorf("asset already assigned")
+	}
+	if err != sql.ErrNoRows {
+		return fmt.Errorf("failed to check existing assignment: %w", err)
+	}
+
+	//inserting into asset_assign table
+	_, err = tx.Exec(`
+		INSERT INTO asset_assign (asset_id, employee_id, assigned_by)
+		VALUES ($1, $2, $3)
+	`, assetId, userId, assignedBy)
+	if err != nil {
+		return fmt.Errorf("failed to insert into asset_assign table :%w", err)
+	}
+	//updating asset status in asset table
+	_, err = tx.Exec(`
+		UPDATE assets SET status = 'assigned' WHERE id = $1
+	`, assetId)
+	if err != nil {
+		return fmt.Errorf("failed to update assignment: %w", err)
+	}
+	return nil
+}
---
+++ handler/manageAssets.go
@@ -0,0 +1,85 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"inventory_management_system/database"
+	"inventory_management_system/database/dbhelper"
+	"inventory_management_system/middlewares"
+	"inventory_management_system/models"
+	"inventory_management_system/utils"
+	"net/http"
+	"strings"
+)
+
+func AssignAssetToUser(w http.ResponseWriter, r *http.Request) {
+	managerId, roles, err := middlewares.GetUserAndRolesFromContext(r)
+	if err != nil {
+		utils.RespondError(w, http.StatusUnauthorized, err, "unauthorized")
+		return
+	}
+	role := roles[0]
+	if role != "admin" && role != "asset_manager" {
+		utils.RespondError(w, http.StatusForbidden, nil, "only admin and asset manager can assign assets")
+		return
+	}
+
+	var assignReq models.AssetAssignReq
+	if err := utils.ParseJSONBody(r, &assignReq); err != nil {
+		utils.RespondError(w, http.StatusBadRequest, err, "invalid request body")
+		return
+	}
+
+	userUUID, err := uuid.Parse(assignReq.UserID)
+	if err != nil {
+		utils.RespondError(w, http.StatusBadRequest, err, "invalid user id")
+		return
+	}
+	assetUUID, err := uuid.Parse(assignReq.AssetID)
+	if err != nil {
+		utils.RespondError(w, http.StatusBadRequest, err, "invalid asset id")
+		return
+	}
+	managerUUID, err := uuid.Parse(managerId)
+	if err != nil {
+		utils.RespondError(w, http.StatusBadRequest, err, "invalid manager id")
+		return
+	}
+
+	tx, err := database.DB.Beginx()
+	if err != nil {
+		utils.RespondError(w, http.StatusInternalServerError, err, "failed to start transaction")
+		return
+	}
+	defer func() {
+		if p := recover(); p != nil || err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
+		}
+	}()
+
+	err = dbhelper.AssignAssetByID(tx, assetUUID, userUUID, managerUUID)
+	if err != nil {
+		if strings.Contains(err.Error(), "already assigned") {
+			utils.RespondError(w, http.StatusConflict, err, "asset is already assigned")
+			return
+		}
+		if strings.Contains(err.Error(), "failed to update assignment") {
+			utils.RespondError(w, http.StatusInternalServerError, err, "failed to assign asset")
+			return
+		}
+		if strings.Contains(err.Error(), "failed to insert into asset_assign table ") {
+			utils.RespondError(w, http.StatusInternalServerError, err, "failed to insert into asset_assign table")
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message":     "asset assigned successfully",
+		"user_id":     userUUID,
+		"asset_id":    assetUUID,
+		"assigned_by": managerUUID,
+	})
+}
