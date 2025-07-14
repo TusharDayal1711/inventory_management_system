8ce99a807f02e52c7ba87762edfa92c50cac56af
TusharDayal1711 <tushar.dayal@remotestate>
2025-07-14 16:18:03 +0530

    add timeline model for user

--- models/timeline.go
+++ models/timeline.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"time"
+)
+
+type UserTimelineRes struct {
+	AssetID      string     `json:"asset_id" db:"asset_id"`
+	Brand        string     `json:"brand" db:"brand"`
+	Model        string     `json:"model" db:"model"`
+	SerialNo     string     `json:"serial_no" db:"serial_no"`
+	AssignedAt   time.Time  `json:"assigned_at" db:"assigned_at"`
+	ReturnedAt   *time.Time `json:"returned_at,omitempty" db:"returned_at"`
+	ReturnReason *string    `json:"return_reason,omitempty" db:"return_reason"`
+}
